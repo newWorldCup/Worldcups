21bef4508a2174f1f37c8b0d4ed6631b111d6fee
injeong Hwang <twilight9758@gmail.com>
2024-02-23 15:29:03 +0900

    초기세팅

---
+++ src/shared/Router.jsx
@@ -0,0 +1,21 @@
+import Detail from 'pages/Detail';
+import Login from 'pages/Login';
+import Main from 'pages/Main';
+import Mypage from 'pages/Mypage';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
