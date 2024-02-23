6e92dd3e01a8fd1652c33d2c1dfdc2e95ec23e56
injeong Hwang <twilight9758@gmail.com>
2024-02-23 15:56:30 +0900

    초기세팅: userSlice.js 추가

---
+++ src/worldCupRedux/modules/userSlice.js
@@ -0,0 +1,11 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const initialState = {
+  users: []
+};
+
+const userSlice = createSlice({
+  name: '',
+  initialState,
+  reducers: {}
+});
