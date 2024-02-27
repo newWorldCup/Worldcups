2dfbc22c56f731450e64b4f2233eb64ffdc79847
KeumSangHo <keumm9612@naver.com>
2024-02-27 14:13:33 +0900

    refactor: style component수정 및 이동

--- src/components/HomeComponents/SearchList.jsx
+++ src/components/HomeComponents/SearchList.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+
 import { collection, getDocs, query } from 'firebase/firestore';
 import { db } from 'firebaseStore/firebaseConfig';
 import useFormInput from 'components/common/useFormInput';
+
+import { InputDiv, SemiContentDiv, ContentDiv, BoxDiv, InfoDiv } from 'styles/StyledSearchList';
+
 const SearchList = () => {
   const [cups, setCups] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   // const [searchTitle.value, setSearchTitle] = useState('');
   const searchTitle = useFormInput('');
+
   useEffect(() => {
     const fetchData = async () => {
       const q = query(collection(db, 'worldCupList'));
@@ -30,7 +34,6 @@ const SearchList = () => {
   }, []);
 
   useEffect(() => {
-    console.log(searchTitle.value);
     if (searchTitle.value !== '') {
       setFilteredData(cups.filter((data) => data.title.toLowerCase().includes(searchTitle.value.toLowerCase())));
     } else {
@@ -60,78 +63,30 @@ const SearchList = () => {
         검색창:&nbsp;
         <input placeholder="제목을 입력해 주세요." value={searchTitle.value} onChange={searchTitle.onChange} />
       </InputDiv>
-      <SemiContentDiv>
-        <ContentDiv>
-          {searchTitle.value === ''
-            ? cups.map((itemList) => {
-                return (
-                  <BoxDiv style={{ backgroundImage: `url(${itemList.thumbNail})` }} key={itemList.id}>
-                    <InfoDiv>
-                      <TitleH3>{itemList.title}</TitleH3>
-                      <ContentP>{itemList.createAt}</ContentP>
-                      <ContentP>작성자:&nbsp;{itemList.userId}</ContentP>
-                    </InfoDiv>
-                  </BoxDiv>
-                );
-              })
-            : filteredData.map((itemList) => {
-                console.log(itemList);
-                return (
-                  <BoxDiv style={{ backgroundImage: `url(${itemList.thumbNail})` }} key={itemList.id}>
-                    <InfoDiv>{itemList.title}</InfoDiv>
-                  </BoxDiv>
-                );
-              })}
-        </ContentDiv>
-      </SemiContentDiv>
+      <ContentDiv>
+        {searchTitle.value === ''
+          ? cups.map((itemList) => {
+              return (
+                <BoxDiv style={{ backgroundImage: `url(${itemList.thumbNail})` }} key={itemList.id}>
+                  <InfoDiv>
+                    <h3>{itemList.title}</h3>
+                    <p>{itemList.createAt}</p>
+                    <p>작성자:&nbsp;{itemList.userId}</p>
+                  </InfoDiv>
+                </BoxDiv>
+              );
+            })
+          : filteredData.map((itemList) => {
+              console.log(itemList);
+              return (
+                <BoxDiv style={{ backgroundImage: `url(${itemList.thumbNail})` }} key={itemList.id}>
+                  <InfoDiv>{itemList.title}</InfoDiv>
+                </BoxDiv>
+              );
+            })}
+      </ContentDiv>
     </>
   );
 };
 
 export default SearchList;
-
-const InputDiv = styled.div`
-  margin: 10px 0px;
-`;
-const SemiContentDiv = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-const ContentDiv = styled.div`
-  justify-content: flex-start;
-  display: flex;
-  margin: 0 auto;
-  flex-wrap: wrap;
-  align-items: center;
-  background-color: skyblue;
-  width: 90%;
-  opacity: 1;
-  -webkit-transition: 0.5s ease-in-out;
-  transition: 0.5s ease-in-out;
-  :hover {
-    filter: brightness(50%);
-  }
-`;
-
-const BoxDiv = styled.div`
-  border: 1px solid blue;
-  border-radius: 5px;
-  width: 200px;
-  height: 250px;
-  margin: 10px;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-`;
-
-const InfoDiv = styled.div`
-  opacity: 0;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  text-transform: uppercase;
-`;
-
-const TitleH3 = styled.h3``;
-
-const ContentP = styled.p``;
