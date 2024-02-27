7edef6b472290fb127ddca1b6198c1c4a7d7eeda
KeumSangHo <keumm9612@naver.com>
2024-02-27 19:02:33 +0900

    fix: 인정님 firebase사용 및 회원가입 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,4 @@
 import SearchList from 'components/HomeComponents/SearchList';
-import styled from 'styled-components';
 import { BodyDiv } from 'styles/StyledMain';
 const Main = () => {
   return (
