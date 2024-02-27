9ad307eeffc47ba82509bb02361320f2c6ae0a62
KeumSangHo <keumm9612@naver.com>
2024-02-27 10:36:16 +0900

    fix: axios.get삭제

--- src/api/queryFns.js
+++
@@ -1,9 +0,0 @@
-// query functions들 선언하시고 export 하시면 됩니다!
-import axios from 'axios';
-
-const getWorldCups = async () => {
-  const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/worldCupList`);
-  return response.data;
-};
-
-export { getWorldCups };
