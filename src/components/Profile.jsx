0cd29c518ba965f4dee68011d205b085029accc4
tjdwns335 <http://github.com/tjdwns335>
2024-02-27 19:34:23 +0900

    fix:myPage 수정

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getDocs } from '@firebase/firestore';
 import YouTube from 'react-youtube';
 import { db } from 'firebaseStore/firebaseConfig';
+import { collection, getDoc, getDocs, query, where } from 'firebase/firestore';
 
 function Profile() {
   const [userMail, setUserMail] = useState(null);
-  // 변수 어떻게 해야 하는지
-  const dataGetFunction = async () => {
-    const data = await getDocs(db, 'worldCupList');
-    return data;
-  };
+  const [worldCupList, setWorldCupList] = useState([]);
+  const nickname = localStorage.getItem('nickname');
+  // 변수 어떻게 해야 하는지 뭘 받아 와야 하나요?
 
   useEffect(() => {
     const auth = getAuth();
@@ -21,35 +19,44 @@ function Profile() {
         setUserMail(null);
       }
     });
-    dataGetFunction();
-  }, []);
-  const { data } = dataGetFunction();
+    const fetchData = async () => {
+      try {
+        // userId가 아직 "추가예정"이라 되는지 모르겠음
+        const q = query(collection(db, 'worldCupList'), where('userId', '==', userMail));
+        // getDoc인지 getDocs인지;;
+        const querySnapshot = await getDoc(q);
+        const data = querySnapshot.docs.map((doc) => doc.data());
+        setWorldCupList(data);
+      } catch (error) {
+        console.error('Error fetching documents:', error);
+      }
+    };
 
-  const filterWorldCupList = data?.filter((item) => item.userId === userMail);
+    if (userMail) {
+      fetchData();
+    }
+  }, [userMail]);
 
   return (
     <div>
       <h2>{userMail}</h2>
-      {filterWorldCupList?.map((item) => {
-        return (
-          <>
-            <div>
-              {/* <div>
-                <img src={item.avatar} alt="profileImg" />
-              </div> */}
-              <span>{userMail}님의 WorldCup-List</span>
-            </div>
-            <div>
-              <h2>{item.videoTitle}</h2>
-              <ul>
-                <li key={item.videoId}>
-                  <YouTube videoId={item.videoId} width="400px" height="300px" />
-                </li>
-              </ul>
-            </div>
-          </>
-        );
-      })}
+      {worldCupList.map((worldCup) => (
+        <div key={worldCup.id}>
+          <div>
+            <span>
+              {nickname}님의 {worldCup.worldCupTitle}
+            </span>
+          </div>
+          <div>
+            {worldCup.videoList.map((video) => (
+              <div key={video.videoId}>
+                <h2>{video.videoTitle}</h2>
+                <YouTube videoId={video.videoId} width="400px" height="300px" />
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SearchList from 'components/HomeComponents/SearchList';
-import styled from 'styled-components';
 import { BodyDiv } from 'styles/StyledMain';
 const Main = () => {
   return (
