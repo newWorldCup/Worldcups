6e2bd9eb5b9540e559763f3774e428376f44c505
gkstnadl <gkstnadl12@naver.com>
2024-02-28 13:30:19 +0900

    Feat : 인트로에 비디오 나오고 끝나면 자동으로 없어지게 구현 완료

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,31 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+// 온클릭했을때 불리언을 true
+// 메인에서 스테이트를 잡아서 비디오로 넘겨주기
 
 const Video = () => {
-  return (
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const video = document.getElementById('video');
+    if (!video) return;
+
+    video.play(); // 비디오 재생
+
+    const onEnd = () => {
+      setIsVisible(false);
+    }; // 비디오 종료 시 실행될 함수
+    video.addEventListener('ended', onEnd);
+
+    return () => video.removeEventListener('ended', onEnd);
+  }, []);
+
+  return isVisible ? (
     <StyledVideoInHome>
-      <Styledvideo autoPlay loop>
-        <source src="/videos/To_my.mp4" type="video/mp4" />
-        <strong>Your browser does not support the video tag.</strong>
-      </Styledvideo>
+      <VideoWrap>
+        <Styledvideo id="video" autoPlay controls muted playsInline>
+          <source src="/videos/To_my.mp4" type="video/mp4" />
+        </Styledvideo>
+      </VideoWrap>
     </StyledVideoInHome>
-  );
+  ) : null;
 };
 
 export default Video;
 
 const StyledVideoInHome = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
+  position: absolute;
   width: 100%;
   height: 100%;
-  overflow: hidden;
-  z-index: -1;
+  top: 0;
+  left: 0;
+  z-index: 100;
+`;
+
+const VideoWrap = styled.div`
+  /* position: relative;
+  height: 100%;
+  overflow: hidden; */
 `;
 
 const Styledvideo = styled.video`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+  position: absolute;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  top: 0;
+  left: 0;
+  transform: translateX(-50%, -50%);
+  @media all and (max-width: 1920px) {
+    width: 100%;
+    height: auto;
+    top: 50%;
+    left: 0;
+    transform: translate(0, -50%);
+  }
 `;
